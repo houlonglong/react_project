53901b92816677e4348c4e8384c4bf18e5f1e810
houlonglong <31363537@qq.com>
2017-04-26 20:51:46 +0800

    适配移动

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -2,27 +2,64 @@ import React from 'react';
 import MobileHerader from './mobile_header.js';
 import MobileFooter from './mobile_footer.js';
 import MoblieList from './moblie_list.js';
-import {
-  Tabs
-} from 'antd';
+import {Tabs, Carousel} from 'antd';
 const TabPane = Tabs.TabPane;
-export default class MobileIndex extends React.Component{
-  render(){
-    return (
-      <div>
-            <MobileHerader />
-             <Tabs>
-                <TabPane tab="头条" key="1">
-                <MoblieList count={20} type="top" />
-                </TabPane>
-                <TabPane tab="社会" key="2"></TabPane>
-                <TabPane tab="国内" key="3"></TabPane>
-                <TabPane tab="国际" key="4"></TabPane>
-                <TabPane tab="娱乐" key="5"></TabPane>
-            </Tabs>
-            <MobileFooter />
-      </div>
+export default class MobileIndex extends React.Component {
 
-    );
-  };
+    render() {
+        let sttings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            skudesToShow: 1,
+            autoplay: true
+        }
+        const banber = <div class="carousel" style={{
+            "float": "none"
+        }}>
+            <Carousel {...sttings}>
+                <div>
+                    <img src="./src/img/carousel_1.jpg"/>
+                </div>
+                <div>
+                    <img src="./src/img/carousel_2.jpg"/>
+                </div>
+                <div>
+                    <img src="./src/img/carousel_3.jpg"/>
+                </div>
+                <div>
+                    <img src="./src/img/carousel_4.jpg"/>
+                </div>
+            </Carousel>
+        </div>
+        return (
+            <div>
+                <MobileHerader/>
+                <Tabs>
+                    <TabPane tab="头条" key="1">
+                        {banber}
+                        <MoblieList count={20} type="top"/>
+                    </TabPane>
+                    <TabPane tab="社会" key="2">
+                        {banber}
+                        <MoblieList count={20} type="shehui"/>
+                    </TabPane>
+                    <TabPane tab="国内" key="3">
+                        {banber}
+                        <MoblieList count={20} type="guonei"/>
+                    </TabPane>
+                    <TabPane tab="国际" key="4">
+                        {banber}
+                        <MoblieList count={20} type="guoji"/>
+                    </TabPane>
+                    <TabPane tab="娱乐" key="5">
+                        {banber}
+                        <MoblieList count={20} type="yule"/>
+                    </TabPane>
+                </Tabs>
+                <MobileFooter/>
+            </div>
+
+        );
+    };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015'],
-          plugins: ['react-html-attrs'], //添加组件的插件配置
+        //  plugins: ['react-html-attrs'], //添加组件的插件配置
+        "plugins": [
+            ["import", { libraryName: "antd", "style": "css" }],
+            ['react-html-attrs'] // `style: true` 会加载 less 文件
+          ]
         }
       },
       //下面是使用 ant-design 的配置文件
